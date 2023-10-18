498be8bea9656b0f5c9142d55e09249c84cbbcaa
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-18 11:12:05 -0500

    Create load page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :entities, foreign_key: :author_id
+  has_many :payments, foreign_key: :author_id
+  has_many :categories, through: :payments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'load#index'
+	devise_for :users
+  
+  devise_scope :user do
+    authenticated :user do
+      root to: 'users#index', as: :authenticated_root
+    end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :load, only: [:index]
+  resources :users, only: [:index, :show]
 end
