bec8c88a7df87f8d3609fe3f4f42d76d3a1e9b9b
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-20 11:00:47 -0500

    Add new category section and styles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,18 +6,15 @@ class CategoriesController < ApplicationController
   end
 
   def new
-    @category = Category.new
-  end
-
-  def total_amount(category)
-    category.payments.sum(:amount)
+    @user = User.find(params[:user_id])
+    @category = @user.categories.build
   end
 
   def create
     @category = current_user.categories.build(category_params)
 
     if @category.save
-      redirect_to root_path
+      redirect_to user_categories_path(current_user)
     else
       render :new, status: :unprocessable_entity
     end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_and_belongs_to_many :payments
+  has_one_attached :icon
+
+  def total_payment_amount
+    payments.sum(:amount)
+  end
 end
